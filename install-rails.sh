9d25c5dc1bc011a298e02aa14c6e91ce79a75a51
filippobm <filippobm@outlook.com>
2020-05-28 00:13:31 +0200

    Create install-rails.sh

---
+++ install-rails.sh
@@ -0,0 +1,21 @@
+# Install git
+sudo apt install git -y
+
+# Install Ruby/Rails
+sudo apt install rbenv -y
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.6.6
+rbenv global 2.6.6
+
+sudo apt install nodejs -y
+sudo apt install yarn -y
+
+gem install bundler
+gem install rails -v 5.2.4.3
