5be81d484ecec9c3259922b228d9ff406280a1b3
filippobm <filippobm@outlook.com>
2020-05-27 23:48:42 +0200

    Create display-ubuntu-ip-in-hyperv.sh

---
+++ display-ubuntu-ip-in-hyperv.sh
@@ -0,0 +1,9 @@
+sudo apt install linux-cloud-tools-common
+sudo apt install linux-tools-generic
+sudo apt install linux-cloud-tools-generic
+
+hv_fcopy_daemon
+hv_kvp_daemon
+hv_set_ifconfig
+hv_vss_daemon
+hv_get_dns_info
