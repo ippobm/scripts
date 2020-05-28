c352561ea5fdd94bdea41d06f1dce52d3b7589de
filippobm <filippobm@outlook.com>
2020-05-28 17:43:25 +0200

    Update install-rails.sh

--- install-rails.sh
+++ install-rails.sh
@@ -11,11 +11,13 @@ sudo -u postgres psql -c "ALTER USER vagrant PASSWORD 'vagrant'"
 # Install Redis
 sudo apt install redis-server -y
 
-# Install Node
-sudo apt install nodejs -y
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-# Install Yarn
-sudo apt install yarn -y
+sudo apt-get update
+sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
 # Install Ruby and Rails
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
