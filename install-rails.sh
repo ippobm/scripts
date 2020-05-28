5352a68f67254ce8260a404e93f72452ebaf465e
filippobm <filippobm@outlook.com>
2020-05-28 20:47:13 +0200

    Update install-rails.sh

--- install-rails.sh
+++ install-rails.sh
@@ -1,18 +1,8 @@
 # Install git
 sudo apt install git -y
 
-# Install PostgreSQL
-sudo apt install postgresql postgresql-contrib libpq-dev -y
-
-# Create PostgreSQL user
-sudo -u postgres createuser vagrant --superuser --createdb --echo 
-sudo -u postgres psql -c "ALTER USER vagrant PASSWORD 'vagrant'"
-
-# Install Redis
-sudo apt install redis-server -y
-
-# Dependencies for Ruby and Rails
-sudo apt install curl
+# Install dependencies for Ruby and Rails
+sudo apt install curl -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
@@ -21,6 +11,7 @@ sudo apt-get update
 sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
 # Install Ruby
+cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
@@ -32,16 +23,31 @@ exec $SHELL
 
 rbenv install 2.6.6
 rbenv global 2.6.6
-
-# Install Nokogiri
-sudo apt-get install libgmp-dev
-sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev
-gem install nokogiri
+ruby -v
 
 # Install Bundler
 gem install bundler
 bundle update
 
 # Install Rails
-gem install rails -v 5.2.4.3
+gem install rails -v 5.2.4.2
 rbenv rehash
+
+# Install Redis
+sudo apt install redis-server -y
+
+# Install PostgreSQL
+sudo apt install postgresql-common -y
+sudo sh /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+
+sudo apt install postgresql postgresql-contrib libpq-dev -y
+sudo apt install pgadmin4 -y
+
+# Create PostgreSQL user
+sudo -u postgres createuser ippo --superuser --createdb --echo 
+sudo -u postgres psql -c "ALTER USER ippo PASSWORD 'syncm13'"
+
+# Install Nokogiri
+sudo apt-get install libgmp-dev
+sudo apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev
+gem install nokogiri
