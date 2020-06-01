b78613e41ab0b545e0bfee6a40d33445d24593e2
filippobm <filippobm@outlook.com>
2020-06-01 02:57:17 +0200

    Update dev-env-setup.sh

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -148,3 +148,7 @@ hv_vss_daemon
 hv_get_dns_info
 
 sudo shutdown -r now
+
+###################################################
+
+update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
